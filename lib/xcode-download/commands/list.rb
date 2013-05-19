be7e2912d0346f450d7cff5e8b1659109e23a384
Ben Chatelain <benchatelain@gmail.com>
2013-05-19 15:20:45 -0600

    Conditionally show tables based on list command arguments
    * all - shows both GUI and CLI
    * gui - shows just GUI
    * cli - shows just CLI
    * <no args> - shows just GUI

--- lib/xcode-download/commands/list.rb
+++ lib/xcode-download/commands/list.rb
@@ -1,38 +1,51 @@
 command :'list' do |c|
   c.syntax = 'xcodedl list'
   c.summary = 'Lists the versions of Xcode available for downloading'
-  c.description = ''
+  c.description = 'Shows only the Xcode GUI versions by default. Specify "all" or "cli" to show command-line tools'
 
   c.action do |args, options|
-    puts "args: #{args}"
+    # puts "args: #{args}"
+
+    show_all = args.include? 'all'
+    show_gui = args.include? 'gui'
+    show_cli = args.include? 'cli'
+    # Show GUI when no args given
+    show_gui = true if !show_all && !show_gui && !show_cli
+
     latest = XcodeDownload::XcodeVersions::LATEST
     gui_versions = XcodeDownload::XcodeVersions::GUI
     cli_versions = XcodeDownload::XcodeVersions::CLI
 
-    title = 'Xcode GUI'
-    table = Terminal::Table.new :title => title do |t|
-      t << ['Version', 'Download URL']
-      gui_versions.keys.each do |version|
-        t << :separator
+    if show_all || show_gui
+      title = 'Xcode GUI'
+      table = Terminal::Table.new :title => title do |t|
+        t << ['Version', 'Download URL']
+        gui_versions.keys.each do |version|
+          t << :separator
 
-        row = [version, gui_versions[version]]
-        t << row
+          row = [version, gui_versions[version]]
+          t << row
+        end
       end
+      puts table
     end
-    puts table
 
-    title = 'Xcode Command-Line'
-    table = Terminal::Table.new :title => title do |t|
-      t << ['Version', 'Download URL']
-      cli_versions.keys.each do |version|
-        t << :separator
+    # Extra line between tables
+    puts if show_all
 
-        row = [version, cli_versions[version]]
-        t << row
+    if show_all || show_cli
+      title = 'Xcode Command-Line'
+      table = Terminal::Table.new :title => title do |t|
+        t << ['Version', 'Download URL']
+        cli_versions.keys.each do |version|
+          t << :separator
+
+          row = [version, cli_versions[version]]
+          t << row
+        end
       end
+      puts table
     end
-    puts
-    puts table
 
   end
 end
