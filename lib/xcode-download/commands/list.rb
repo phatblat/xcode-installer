bc7778a07710525d447e032e24ede2a9635d3f97
Ben Chatelain <benchatelain@gmail.com>
2013-05-19 15:08:19 -0600

    List CLI versions and add URL column

--- lib/xcode-download/commands/list.rb
+++ lib/xcode-download/commands/list.rb
@@ -4,21 +4,35 @@ command :'list' do |c|
   c.description = ''
 
   c.action do |args, options|
+    puts "args: #{args}"
     latest = XcodeDownload::XcodeVersions::LATEST
     gui_versions = XcodeDownload::XcodeVersions::GUI
     cli_versions = XcodeDownload::XcodeVersions::CLI
 
     title = 'Xcode GUI'
     table = Terminal::Table.new :title => title do |t|
-      # t << ["Version"]
+      t << ['Version', 'Download URL']
       gui_versions.keys.each do |version|
-        # t << :separator
+        t << :separator
 
-        row = [version]
+        row = [version, gui_versions[version]]
         t << row
       end
     end
-
     puts table
+
+    title = 'Xcode Command-Line'
+    table = Terminal::Table.new :title => title do |t|
+      t << ['Version', 'Download URL']
+      cli_versions.keys.each do |version|
+        t << :separator
+
+        row = [version, cli_versions[version]]
+        t << row
+      end
+    end
+    puts
+    puts table
+
   end
 end
