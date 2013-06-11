a557fa033f55fbe7f8dd94ec8b4e028c63655aaf
Ben Chatelain <benchatelain@gmail.com>
2013-06-10 17:09:41 -0700

    Add caveat to install command

--- lib/xcode-installer/commands/install.rb
+++ lib/xcode-installer/commands/install.rb
@@ -2,7 +2,7 @@ command :'install' do |c|
   c.syntax = 'xcode-installer install [options]'
   c.option '--no-trash', 'Prevents trashing .dmg after install'
   c.summary = 'Installs xcode from a previous download'
-  c.description = ''
+  c.description = 'NEEDS SOME WORK - Use download and mount .dmg manually for now'
 
   c.action do |args, options|
     files = Dir.glob('*.dmg')
