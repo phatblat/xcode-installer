05ff7800695130e0c41f8c6e66ea8a781788c357
Ben Chatelain <ben.d.chatelain@kp.org>
2013-07-29 15:39:24 -0600

    Update appIdKey in URL for ADC login form

--- lib/xcode-installer/apple-developer-center.rb
+++ lib/xcode-installer/apple-developer-center.rb
@@ -3,7 +3,7 @@ module XcodeInstaller
 
     HOST = "developer.apple.com"
 
-    LOGIN_URL = 'https://daw.apple.com/cgi-bin/WebObjects/DSAuthWeb.woa/wa/login?appIdKey=d4f7d769c2abecc664d0dadfed6a67f943442b5e9c87524d4587a95773750cea&path=%2F%2Fdownloads%2Findex.action'
+    LOGIN_URL = 'https://daw.apple.com/cgi-bin/WebObjects/DSAuthWeb.woa/wa/login?appIdKey=891bd3417a7776362562d2197f89480a8547b108fd934911bcbea0110d07f757&path=%2F%2Fdownloads%2Findex.action'
     DOWNLOADS_URL = 'https://developer.apple.com/downloads/index.action'
 
   end
