ac2cbad33053941252edff5646523111ad9da3c1
Ben Chatelain <benchatelain@gmail.com>
2013-08-06 21:45:11 -0600

    Add optional args to list command help syntax

--- lib/xcode-installer/commands/list.rb
+++ lib/xcode-installer/commands/list.rb
@@ -1,5 +1,5 @@
 command :'list' do |c|
-  c.syntax = 'xcode-installer list'
+  c.syntax = 'xcode-installer list [all|gui|cli]'
   c.summary = 'Lists the versions of Xcode available for downloading'
   c.description = 'Shows only the Xcode GUI versions by default. Specify "all" or "cli" to show command-line tools'
 
