9fa8d40aad1cfd75f0cd97dcfe51901aff2e3c6c
Ben Chatelain <benchatelain@gmail.com>
2013-09-23 16:41:22 -0600

    Refactor download command into class

--- lib/xcode-installer/commands/download.rb
+++ lib/xcode-installer/commands/download.rb
@@ -1,3 +1,5 @@
+require 'xcode-installer/download'
+
 command :'download' do |c|
   c.syntax = 'xcode-installer download [options]'
   c.option '--dry-run', 'Enables a HEAD request instead of downloading the file'
@@ -6,35 +8,5 @@ command :'download' do |c|
   c.summary = 'Initiates the download'
   c.description = ''
 
-  c.action do |args, options|
-    if options.release
-      xcode_version = options.release
-    else
-      if options.pre_release
-        xcode_version = XcodeInstaller::XcodeVersions::LATEST_DP
-      else
-        xcode_version = XcodeInstaller::XcodeVersions::LATEST_GA
-      end
-    end
-
-    mgr = XcodeInstaller::XcodeVersions::ReleaseManager.new
-    release = mgr.get_release(xcode_version, options.pre_release, 'gui')
-
-    if release
-      xcode_url = release['download_url']
-    else
-      puts "No Xcode release with number #{xcode_version}. Use the 'list' command to see a list of known releases."
-      exit
-    end
-
-    puts "Downloading Xcode #{xcode_version}"
-    puts xcode_url
-
-    agent.verbose = options.verbose
-    agent.dry_run = options.dry_run
-    try {
-      filename = agent.download(xcode_url)
-      puts "File saved to: #{Dir.pwd}/#{filename}" if filename
-    }
-  end
+  c.action XcodeInstaller::Download, :action
 end
