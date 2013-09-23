ce32234ce445c4652937eaa195b1f71c45d2ef09
Ben Chatelain <benchatelain@gmail.com>
2013-09-23 16:52:35 -0600

    Refactor install command into class

--- lib/xcode-installer/commands/install.rb
+++ lib/xcode-installer/commands/install.rb
@@ -1,33 +1,10 @@
+require 'xcode-installer/install'
+
 command :'install' do |c|
   c.syntax = 'xcode-installer install [options]'
   c.option '--no-trash', 'Prevents trashing .dmg after install'
   c.summary = 'Installs xcode from a previous download'
   c.description = 'NEEDS SOME WORK - Use download and mount .dmg manually for now'
 
-  c.action do |args, options|
-    files = Dir.glob('*.dmg')
-    if files.length == 0
-      puts 'No .dmg files found in current directory. Run the download command first.'
-      return
-    elsif files.length > 1
-      puts 'Multiple .dmg files found in the current directory. Currently no support for specifying file.'
-      return
-    end
-    dmg_file = files[0]
-
-    # Mount disk image
-    mountpoint = '/Volumes/Xcode'
-    # system "hdid '#{dmg_file}' -mountpoint #{mountpoint}"
-    system 'hdiutil attach -quiet xcode4620419895a.dmg'
-
-    # Trash existing install (so command is rerunnable)
-    destination = '/Applications/Xcode.app'
-    Trash.new.throw_out(destination)
-
-    # Copy into /Applications
-    puts 'Copying Xcode.app into Applications directory (this can take a little while)'
-    system "cp -R #{mountpoint}/Xcode.app #{destination}"
-
-    system 'hdiutil detach -quiet #{mountpoint}'
-  end
+  c.action XcodeInstaller::Install, :action
 end
---
+++ lib/xcode-installer/install.rb
@@ -0,0 +1,47 @@
+#
+# XcodeInstaller::Install (Command Class)
+#
+# First, this command will look for the expected version of the Xcode installation .dmg file in the download (current) dir.
+# If not found, the download will be initiated, afterwards the expected file will be verified.
+# Installation will continue once the expected .dmg file is found. The .dmg will be mounted and the .app file copied out.
+# By default, the version number will be inserted into the Xcode.app name. This will prevent collision with the Mac App Store
+# installed version. An option may allow for this utility to overwrite the Xcode.app so that there is only a single copy installed.
+# It would be nice to also set the newly installed Xcode to be the default on the command line using 'xcode-select --switch'.
+#
+
+require 'xcode-installer/download'
+
+module XcodeInstaller
+  class Install
+
+    def action(args, options)
+      puts 'inside XcodeInstaller::Install.action'
+
+      files = Dir.glob('*.dmg')
+      if files.length == 0
+        puts 'No .dmg files found in current directory. Run the download command first.'
+        return
+      elsif files.length > 1
+        puts 'Multiple .dmg files found in the current directory. Currently no support for specifying file.'
+        return
+      end
+      dmg_file = files[0]
+
+      # Mount disk image
+      mountpoint = '/Volumes/Xcode'
+      # system "hdid '#{dmg_file}' -mountpoint #{mountpoint}"
+      system 'hdiutil attach -quiet xcode4620419895a.dmg'
+
+      # Trash existing install (so command is rerunnable)
+      destination = '/Applications/Xcode.app'
+      Trash.new.throw_out(destination)
+
+      # Copy into /Applications
+      puts 'Copying Xcode.app into Applications directory (this can take a little while)'
+      system "cp -R #{mountpoint}/Xcode.app #{destination}"
+
+      system 'hdiutil detach -quiet #{mountpoint}'
+    end
+
+  end
+end
