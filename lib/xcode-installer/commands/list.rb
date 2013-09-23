287e7461435da1cc190a715bc12e0d05cec06180
Ben Chatelain <benchatelain@gmail.com>
2013-09-23 16:45:53 -0600

    Refactor list command into class

--- lib/xcode-installer/commands/list.rb
+++ lib/xcode-installer/commands/list.rb
@@ -1,54 +1,9 @@
+require 'xcode-installer/list'
+
 command :'list' do |c|
   c.syntax = 'xcode-installer list [all|gui|cli]'
   c.summary = 'Lists the versions of Xcode available for downloading'
   c.description = 'Shows only the Xcode GUI versions by default. Specify "all" or "cli" to show command-line tools'
 
-  c.action do |args, options|
-    # puts "args: #{args}"
-
-    show_all = args.include? 'all'
-    show_gui = args.include? 'gui'
-    show_cli = args.include? 'cli'
-    # Show GUI when no args given
-    show_gui = true if !show_all && !show_gui && !show_cli
-
-    # latest = XcodeInstaller::XcodeVersions::LATEST
-    mgr = XcodeInstaller::XcodeVersions::ReleaseManager.new
-
-    gui_versions = mgr.get_all('gui')
-    cli_versions = mgr.get_all('cli')
-    puts cli_versions
-
-    if show_all || show_gui
-      title = 'Xcode GUI'
-      table = Terminal::Table.new :title => title do |t|
-        t << ['Version', 'Download URL']
-        gui_versions.each do |release|
-          t << :separator
-
-          row = [release['version'], release['download_url']]
-          t << row
-        end
-      end
-      puts table
-    end
-
-    # Extra line between tables
-    puts if show_all
-
-    if show_all || show_cli
-      title = 'Xcode Command-Line'
-      table = Terminal::Table.new :title => title do |t|
-        t << ['Version', 'Download URL']
-        cli_versions.each do |release|
-          t << :separator
-
-          row = [release['version'], release['download_url']]
-          t << row
-        end
-      end
-      puts table
-    end
-
-  end
+  c.action XcodeInstaller::List, :action
 end
---
+++ lib/xcode-installer/list.rb
@@ -0,0 +1,55 @@
+#
+# XcodeInstaller::List
+#
+
+module XcodeInstaller
+  class List
+
+    def action(args, options)
+
+      show_all = args.include? 'all'
+      show_gui = args.include? 'gui'
+      show_cli = args.include? 'cli'
+      # Show GUI when no args given
+      show_gui = true if !show_all && !show_gui && !show_cli
+
+      # latest = XcodeInstaller::XcodeVersions::LATEST
+      mgr = XcodeInstaller::XcodeVersions::ReleaseManager.new
+
+      gui_versions = mgr.get_all('gui')
+      cli_versions = mgr.get_all('cli')
+
+      if show_all || show_gui
+        title = 'Xcode GUI'
+        table = Terminal::Table.new :title => title do |t|
+          t << ['Version', 'Download URL']
+          gui_versions.each do |release|
+            t << :separator
+
+            row = [release['version'], release['download_url']]
+            t << row
+          end
+        end
+        puts table
+      end
+
+      # Extra line between tables
+      puts if show_all
+
+      if show_all || show_cli
+        title = 'Xcode Command-Line'
+        table = Terminal::Table.new :title => title do |t|
+          t << ['Version', 'Download URL']
+          cli_versions.each do |release|
+            t << :separator
+
+            row = [release['version'], release['download_url']]
+            t << row
+          end
+        end
+        puts table
+      end
+    end
+
+  end
+end
