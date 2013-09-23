066386dda2db64f045ca7c000c06029e364f6c71
Ben Chatelain <benchatelain@gmail.com>
2013-09-23 17:27:10 -0600

    Remove redundnant version determination logic from download command

--- lib/xcode-installer/download.rb
+++ lib/xcode-installer/download.rb
@@ -7,27 +7,17 @@ module XcodeInstaller
   class Download
 
     def action(args, options)
-      if options.release
-        xcode_version = options.release
-      else
-        if options.pre_release
-          xcode_version = XcodeInstaller::XcodeVersions::LATEST_DP
-        else
-          xcode_version = XcodeInstaller::XcodeVersions::LATEST_GA
-        end
-      end
-
       mgr = XcodeInstaller::ReleaseManager.new
-      release = mgr.get_release(xcode_version, options.pre_release)
+      release = mgr.get_release(nil, options.pre_release)
 
       if release
         xcode_url = release['download_url']
       else
-        puts "No Xcode release with number #{xcode_version}. Use the 'list' command to see a list of known releases."
+        puts "No Xcode release with number #{options.release}. Use the 'list' command to see a list of known releases."
         exit
       end
 
-      puts "Downloading Xcode #{xcode_version}"
+      puts "Downloading Xcode #{release['version']}"
       puts xcode_url
 
       agent.verbose = options.verbose
