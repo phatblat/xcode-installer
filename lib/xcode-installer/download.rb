50ea4bc8b72ea52ccd81c181c9f1a550a872abf7
Ben Chatelain <benchatelain@gmail.com>
2013-09-23 20:02:39 -0600

    Store release information in a property and ensure to pass the requested release down to ReleaseManager

--- lib/xcode-installer/download.rb
+++ lib/xcode-installer/download.rb
@@ -1,23 +1,25 @@
 #
 # XcodeInstaller::Download (Command Class)
 #
+# Determines the requested Xcode release (with the help of the ReleaseManager class) and handles the download, showing a progress bar with ETA.
 #
 
 module XcodeInstaller
   class Download
+    attr_accessor :release
 
     def action(args, options)
       mgr = XcodeInstaller::ReleaseManager.new
-      release = mgr.get_release(nil, options.pre_release)
+      @release = mgr.get_release(options.release, options.pre_release)
 
       if release
-        xcode_url = release['download_url']
+        xcode_url = @release['download_url']
       else
         puts "No Xcode release with number #{options.release}. Use the 'list' command to see a list of known releases."
         exit
       end
 
-      puts "Downloading Xcode #{release['version']}"
+      puts "Downloading Xcode #{@release['version']}"
       puts xcode_url
 
       agent.verbose = options.verbose
